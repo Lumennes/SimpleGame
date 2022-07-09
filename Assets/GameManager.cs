2089256432a8a8b7847d1f908d5896c14c7e0035
Lumennes <lumennes@gmail.com>
2022-07-10 00:08:26 +0500

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSecondsRealtime(5);
+
+        text.text = "Новый текст";
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
